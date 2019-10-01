f42ad2e283c55241217204cd84ab4460b4fb881c
VoLeKhanhDuy <50372225+VoLeKhanhDuy@users.noreply.github.com>
2019-10-01 09:23:24 +0700

    Add files via upload

---
+++ Bai_1.java
@@ -0,0 +1,20 @@
+package Bai_1;
+
+public class Bai_1 {
+	/*
+	 Thiết kế lớp Employee với các thuộc tính: id(int), name(string), basic_sal, email, phone
+	 a) Viết các get, set
+	 b) Viết phương thức nhập thông tin Employee từ bàn phím
+	 c) Viết phương thức tính lương tổng: "Sum_of_sal". Phương thức nhận một tham số nguyên 
+	 	index: Lương tổng = basic_sal*index. Phương thức trả về lương tổng
+	 d) Viết phương thức in thông tin 1 employee ra màn hình. Phương thức có tham số là employee cần in
+	 e) Viết thêm 1 constructor cho phép thiết lập tất cả các giá trị thuộc tính của employee
+	 		
+	 */
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
