bb4424822fbbe8c1120bdb84136b3c0472c27f15
= <=>
2019-10-22 07:00:12 +0700

    BT update 16-10

--- BT_16-10-2019/BT_16-10-2019/src/BT_2/Bai2.java
+++ BT_16-10-2019/BT_16-10-2019/src/BT_2/Bai2.java
@@ -1,18 +1,42 @@
 package BT_2;
 
+import java.util.Scanner;
+
 public class Bai2 {
 
 	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int size;
+		System.out.print("Nhập vào số lượng phần tử: ");
+		size = scan.nextInt();
+		int a[] = new int[size];
+	
+		for(int i = 0; i < a.length; i++)
+		{
+			System.out.print("Nhập vào phần tử thứ " + i + " : ");
+			a[i] = scan.nextInt();
+		}
 		
-
+		Bai2 sn = new Bai2();
+		System.out.println("Gía trị nhỏ nhất trong mảng là: " + sn.min(a));
+		System.out.print("Nhập vào giá trị cần tìm phần tử trong mảng: ");
+		int k = scan.nextInt();
+		if(find(a, k) == 0) System.out.println("Không tìm thấy giá trị xuất hiện đầu tiên trong mảng");
+		else System.out.println("Gía trị phần tử xuất hiện đầu tiên trong mảng của "  + k + " là " + sn.find(a, k));
+		
+		System.out.print("Tổng của tất cả phần tử trong mảng: " + sn.sumArray(a));
+		System.out.print("\nMảng có các giá trị là: ");
+		sn.printArray(a);
+		System.out.print("\nMảng đối xứng: ");
+		sn.reverseArray(a);
 	}
 	
 	
 	//Tra ve gia tri nho nhat luu tru trong mang
-	public static int min(int mang[], int n)
+	public static int min(int mang[])
 	{
 		 int min = mang[0];
-		 for(int i = 0; i < n; i++)
+		 for(int i = 0; i < mang.length; i++)
 		 {
 			 if(mang[i] < min)
 				 min = mang[i];
@@ -21,23 +45,25 @@ public class Bai2 {
 	}
 	
 	//Tra ve vi tri xuat hien dau tien cua gia tri trong mang
-	public static int find(int mang[], int value, int n)
+	public static int find(int mang[], int value)
 	{
-		for(int i = 0; i < n; i++)
+		int flag = 1;
+		for(int i = 0; i < mang.length; i++)
 		{
 			if(mang[i] == value) 
-				return i;
-			break;
+			{
+				flag = i;
+				break;
+			}
 		}
-		return -1;
-		
+		return flag;
 	}
 	
 	//Tra ve tong cua tat ca phan tu trong mang
-	public static int sumArray(int mang[], int n)
+	public static int sumArray(int mang[])
 	{
 		int tong = 0;
-		for(int i = 0; i < n; i++)
+		for(int i = 0; i < mang.length; i++)
 		{
 			tong += mang[i];
 		}
@@ -45,8 +71,29 @@ public class Bai2 {
 	}
 	
 	// In cac gia tri duoc luu trong mang
+	public void printArray(int mang[])
+	{
+		for(int i = 0; i < mang.length; i++)
+		{
+			System.out.print(mang[i] + "  ");
+		}
+	}
 	
 	// Tra ve 1 mang moi luu tru cac phan tu cua mang duoc dua lam doi so
+	public void reverseArray(int mang[])
+	{
+		for(int i = 0, j = mang.length - 1; i <= j; i++, j--)
+		{
+			int tam = mang[i];
+			mang[i] = mang[j];
+			mang[j] = tam;
+		}
+		
+		for(int i = 0; i < mang.length; i++) {
+			System.out.print(mang[i] + "  ");
+		}
+	
+	}
 	
 
 }
--- BT_16-10-2019/BT_16-10-2019/src/BT_3/Bai3.java
+++ BT_16-10-2019/BT_16-10-2019/src/BT_3/Bai3.java
@@ -6,24 +6,25 @@ public class Bai3 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("Kết quả trả về là: " + soNguyen());
 	}
 	
-	public static int soNguyen(int a, int b)
+	
+	
+	public static int soNguyen()
 	{
-		int tong, tich;
+		int a,b;
 		Scanner scan = new Scanner(System.in);
-		System.out.println("Nhap vao a: ");
+		System.out.println("Nhập vào a: ");
 		a = scan.nextInt();
-		System.out.println("Nhap vao b: ");
+		System.out.println("Nhập vào b: ");
 		b = scan.nextInt();
 		if(a %2 == 0 && b %2 == 0)
-			return tong;
+			return a + b;
 		else if (a %2 != 0 && b %2 != 0)
-			return tich;
-		else
-			return
-		
+			return a * b;
+		else  return (int)Math.pow(a,2) + (int)Math.pow(b,2);
 	}
 
 }
+
